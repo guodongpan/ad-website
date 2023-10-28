d8365634c2d8ea750dd0d2e868ada3499549097c
pan <3220390084@qq.com>
2023-10-28 12:49:01 +0800

    cicd

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   // baseUrl: '/',
-  baseUrl: isProduct ? '/ad-website' : "/",
+  baseUrl: isProduct ? '/ad-website' : "/ad-website",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
